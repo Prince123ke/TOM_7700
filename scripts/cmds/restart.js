716a03b7a47e11dd5bc2becb451fd0743ea0769e
TOM-7700 <alamindx97@icloud.com>
2025-07-10 10:16:40 +0300

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -28,7 +28,10 @@ module.exports = {
 	},
 
 	onLoad: function ({ api }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
+		const tmpDir = `${__dirname}/tmp`;
+		const pathFile = `${tmpDir}/restart.txt`;
+		// ফোল্ডার অটো তৈরি করবে
+		fs.ensureDirSync(tmpDir);
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
 			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
@@ -37,7 +40,11 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, event, getLang }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
+		const tmpDir = `${__dirname}/tmp`;
+		// ফোল্ডার অটো তৈরি করবে
+		fs.ensureDirSync(tmpDir);
+
+		const pathFile = `${tmpDir}/restart.txt`;
 		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
 		await message.reply(getLang("restartting"));
 		process.exit(2);
